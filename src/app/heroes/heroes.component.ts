0455179c2066537711f7f3fce8d06ded92c3a33d
Saarthak Sharma <sharma.saarthak@gmail.com>
2020-06-18 18:41:25 -0700

    added routing and dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,13 +10,13 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes : Hero[];
-  selectedHero: Hero;
+  // selectedHero: Hero;
   
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-    console.log(hero.name);
-  }
+  // onSelect(hero: Hero): void{
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  //   console.log(hero.name);
+  // }
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
